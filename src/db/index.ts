a9f3558e3bfbc0bae1705a5479a266518beb3456
Muhammad Falich Farisy <muhammadfalich@gmail.com>
2026-03-20 14:38:49 +0700

    chore: remove old root-level project files

--- src/db/index.ts
+++
@@ -1,7 +0,0 @@
-import { drizzle } from "drizzle-orm/bun-sqlite";
-import { Database } from "bun:sqlite";
-import * as schema from "./schema";
-
-const sqlite = new Database("ecommerce.db");
-export const db = drizzle(sqlite, { schema });
-export * from "./schema";
