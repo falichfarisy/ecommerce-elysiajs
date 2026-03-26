00b7986d72c9630562ace45fb7e637d12b1c0c7e
Muhammad Falich Farisy <muhammadfalich@gmail.com>
2026-03-26 11:06:37 +0700

    chore: remove unused files and simplify globals.css

--- frontend/app/api/api_register.ts
+++ frontend/app/api/api_register.ts
@@ -1,3 +1,8 @@
-class apiRegister {
-    
-}
+// import { env } from "process";
+// import { postData } from "../api_config";
+
+// export default function postRegister(name, )
+
+// const response = postData(process.env.API_REGISTER as string, data:{
+//     'nama'
+// })
